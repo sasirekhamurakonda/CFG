a43ab22a6073a5158629774bac7beb152fae5f64
unknown <atmakursreemeghana22@gmail.com>
2020-08-15 23:14:21 +0530

    basic files

--- login.php
+++ login.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
-
+<head>
+        <title>CFG PROJECT</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
     <body>
 <div class="navbar navbar-default ">
     <div class="container">
-		<a class="navbar-brand" href="index.php">Laptop-Store</a>
+		<a class="navbar-brand" href="index.php">CFG Event</a>
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
