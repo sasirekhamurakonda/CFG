d3d97f4b8eaeebd97abcb7fd7a851a23472e194c
unknown <atmakursreemeghana22@gmail.com>
2020-08-16 18:09:50 +0530

    new files

---
+++ js/demo/main.js
@@ -0,0 +1,46 @@
+var exportData;
+var yearsArray=[];
+var exportsArray=[];
+$(document).ready(function(){
+    getExportData();
+    
+});
+function getExportData(){
+    /*$.getJSON("http://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0", function(result){
+    $.getJSON("http://106.0.36.170:8080/just/json.php", function(result){
+        sampleData=result; 
+        console.log(sampleData);
+    updateChartArea(); 
+    });*/
+    $.ajax({
+        type: "GET",
+        url: 'http://localhost/just/json.php',
+        dataType: 'json',
+        success: function (obj, textstatus) {
+                      if( !('error' in obj) ) {
+                          exportData = obj;
+                          console.log(obj);
+                          updateChartArea("India")
+                      }
+                      else {
+                          console.log(obj.error);
+                      }
+                }
+    });
+}
+function updateChartArea(countryName){
+    console.log(exportData)
+    $.each(exportData,function(index,value){
+    if(value.area=="India")
+        {
+           
+            yearsArray.push(value.year)
+            exportsArray.push(parseInt(value.population))
+        }
+       
+
+      })
+      console.log(yearsArray)
+    console.log(exportsArray)
+    drawExportChart();
+}
--- json.php
+++ json.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>  
- <html>  
-      <head>  
-           <title>Convert Data from Mysql to JSON Format using PHP</title>  
-      </head>  
-      <body>  
+ 
            <?php   
+		   header('Content-Type: application/json');
            $connect = mysqli_connect("localhost", "root", "", "eggs");  
+		   
            $sql = "SELECT area ,element ,year ,population from `population` WHERE element='Female' and year > '1999'";  
            $result = mysqli_query($connect, $sql);  
            $json_array = array();  
@@ -15,5 +12,3 @@
            }   
            echo json_encode($json_array);  
            ?>  
-      </body>  
- </html>  
