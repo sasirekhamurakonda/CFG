a1b120874180c2c270bf075f682c2ad2d4968884
unknown <atmakursreemeghana22@gmail.com>
2020-08-16 15:24:43 +0530

    Backend

---
+++ json3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Convert Data from Mysql to JSON Format using PHP</title>  
+      </head>  
+      <body>  
+           <?php   
+           $connect = mysqli_connect("localhost", "root", "", "eggs");  
+           $sql = "SELECT * from supply2";  
+           $result = mysqli_query($connect, $sql);  
+           $json_array = array();  
+           while($row = mysqli_fetch_assoc($result))  
+           {  
+                $json_array[] = $row;  
+           }   
+           echo json_encode($json_array); 
+           ?>  
+      </body>  
+ </html>  
---
+++ supply2.php
@@ -0,0 +1,45 @@
+<?php  
+$connect = mysqli_connect("localhost", "root", "", "eggs");
+if(isset($_POST["submit"]))
+{
+ if($_FILES['file']['name'])
+ {
+  $filename = explode(".", $_FILES['file']['name']);
+  if($filename[1] == 'csv')
+  {
+   $handle = fopen($_FILES['file']['name'], "r");
+   while($data = fgetcsv($handle))
+   {
+    $item0 = mysqli_real_escape_string($connect, $data[0]);  
+    $item1 = mysqli_real_escape_string($connect, $data[1]);
+	    $item2 = mysqli_real_escape_string($connect, $data[2]);
+    $item3 = mysqli_real_escape_string($connect, $data[3]);
+	
+
+                $query = "INSERT into supply2(year, tradeflow, reporter , unitprice)
+				values('$item0','$item1','$item2','$item3')";
+                mysqli_query($connect, $query);
+   }
+   fclose($handle);
+   echo "<script>alert('Import done');</script>";
+  }
+ }
+}
+?>  
+<!DOCTYPE html>  
+<html>  
+ <head>  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+ </head>  
+ <body>  
+  <form method="post" enctype="multipart/form-data">
+   <div align="center">  
+    <label>Select CSV File:</label>
+    <input type="file" name="file" />
+    <br />
+    <input type="submit" name="submit" value="Import" class="btn btn-info" />
+   </div>
+  </form>
+ </body>  
+</html>
